304b042146ef4fefb36490971a674cf04c2cffeb
fynboiozzy08-cmd <fynboiozzy08@gmail.com>
2026-09-17 17:48:31 +0100

    Change SUPABASE_KEY in config.js
    Updated SUPABASE_KEY in the configuration.

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 window.LEO_CONFIG = {
   SUPABASE_URL: "https://odpmhwmnzetqgniplted.supabase.co",
-  SUPABASE_KEY: "sb_publishable_2XdLA65q6X2oRPd1T-rIiw_yc5J9bqF"
+  SUPABASE_KEY: "sb_publishable_92LU7ZNyugh6GfjtVhsPLg_FlvMxaq"
 };
+
+console.log(
+  "LEO KEY LOADED:",
+  window.LEO_CONFIG.SUPABASE_KEY.startsWith("sb_publishable_"),
+  window.LEO_CONFIG.SUPABASE_KEY.length
+);
