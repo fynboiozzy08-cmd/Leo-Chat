ea5411655a3a9d945c6f182e8ab141034d0e573f
fynboiozzy08-cmd <fynboiozzy08@gmail.com>
2026-09-17 16:02:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.LEO_CONFIG = {
   SUPABASE_URL: "https://odpmhwmnzetqgniplted.supabase.co",
-  SUPABASE_KEY: "sb_publishable_..."
+  SUPABASE_KEY: "sb_publishable_2XdLA65q6X2oRPd1T-rIiw_yc5J9bqF"
 };
