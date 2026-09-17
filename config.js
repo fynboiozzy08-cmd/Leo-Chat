0dd42d4398713f99f2c72150a82f04ff4d7cb2d0
fynboiozzy08-cmd <fynboiozzy08@gmail.com>
2026-09-17 15:30:27 +0100

    Obfuscate SUPABASE_KEY in config.js
    Updated SUPABASE_KEY to a more secure format.

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.LEO_CONFIG = {
   SUPABASE_URL: "https://odpmhwmnzetqgniplted.supabase.co",
-  SUPABASE_KEY: "sb_publishable_92LU7ZNyugh6GfjtVhsPLg_FlvOMxaq"
+  SUPABASE_KEY: "sb_publishable_..."
 };
