da1551743c52ae57b8ac74bf8f7e389e6670fb6c
fynboiozzy08-cmd <fynboiozzy08@gmail.com>
2026-09-19 02:31:46 +0100

    Refactor service worker for simplified caching

--- sw.js
+++ sw.js
@@ -1,63 +1,23 @@
-const CACHE = "leo-chat-v7-2";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./config.js",
-  "./manifest.webmanifest",
-  "./logo.svg"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches
-      .open(CACHE)
-      .then((cache) => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) =>
-        Promise.all(
-          cacheNames
-            .filter((name) => name !== CACHE)
-            .map((name) => caches.delete(name))
-        )
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => caches.delete(key))
       )
-      .then(() => self.clients.claim())
+    )
   );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request)
-        .then((response) => {
-          if (!response || response.status !== 200) {
-            return response;
-          }
-
-          const copy = response.clone();
-
-          caches.open(CACHE).then((cache) => {
-            cache.put(event.request, copy);
-          });
-
-          return response;
-        })
-        .catch(() => cachedResponse);
-    })
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
   );
 });
